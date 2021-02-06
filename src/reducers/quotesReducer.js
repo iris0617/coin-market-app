60a8d29fd1f58abf49563ce180542565844d2f80
Iris Li <irisli@Iriss-MacBook-Pro.local>
2021-02-06 11:01:40 -0500

    update design pdf

--- src/reducers/quotesReducer.js
+++ src/reducers/quotesReducer.js
@@ -5,7 +5,7 @@ const initialState={ list: {}, error: [] };
 const QuotesReducer = (state=initialState, action) => {
     switch(action.type){
         case quotesActions.GET_QUOTE_INFO_SUCCESS:
-            return { ...state, list: {...action.payload}, error: [] }
+            return { ...state, list: {...state.list, ...action.payload}, error: [] }
         case quotesActions.SET_QUOTE_INFO_ERROR:
             return { ...state, list: {}, error: [...state.error, action.payload] }
         default:
